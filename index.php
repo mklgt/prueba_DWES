5ac496661a30e614a679dc16b9f9bc35515b68fa
Asier Gonzalez <agonzalgam1@educacion.navarra.es>
2021-10-29 10:40:29 +0200

    Colaborador: asier:subiendo los archivos

---
+++ index.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+// 1º Admin: Inicio proyecto 
+// include "helper/funciones.php";
+
+// 2º Colaborador: incluir la función calcularArea() con la rama: funccalculararea
+// $resultado = "<h3>Cálculo del área de un cuadrado</h3>";
+// $lado = 7;
+// $area = calcularAreaCuadrado($lado); 
+// $resultado .= "<p>El &aacute;rea de un cuadrado de lado $lado es $area</p>";
+
+
+// 3º Admin:incluir la función calcularMedia3() con la rama: funccalcularmedia
+// $resultado.= "<h3>La media de 8, 12 y 25 es ".calcularMedia3(8, 12, 25)."</h3>"; 
+
+// $resultado .= mostrarMensajeh3("Ejemplo de función sin valor de retorno"); 
+// $resultado.= mostrarLinHorizontal(); 
+
+
+//include "vistas/vista_resultado.php";
