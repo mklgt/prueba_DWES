eb36591c4816988daa76f22d26782a529bae997b
Asier Gonzalez <agonzalgam1@educacion.navarra.es>
2021-10-29 10:49:38 +0200

    Colaborador:Funciones

---
+++ helper/funciones.php
@@ -0,0 +1,17 @@
+<?php
+      /*
+       * función que calcula el área de un cuadrado
+       */
+      function calcularAreaCuadrado($lado)
+      {
+          $area = $lado * $lado;
+          return $area;
+      }
+      /*
+       * función que calcula la media
+       */
+      function calcularMedia3($num1, $num2, $num3)
+      {
+          return ($num1 + $num2 + $num3) / 3;
+      }
+?>
